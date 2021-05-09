8989c7d724a4335e4b55a737701c218a80522145
Kamil Piękoś <kamcio190@gmail.com>
2021-05-09 03:05:36 +0200

    created first exercise

---
+++ lab1/Z1/Z1/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿
+namespace Z1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.xInput = new System.Windows.Forms.TextBox();
+            this.yInput = new System.Windows.Forms.TextBox();
+            this.zInput = new System.Windows.Forms.TextBox();
+            this.resultInput = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.addButton = new System.Windows.Forms.Button();
+            this.multiplyButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "x";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "y";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "result";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(12, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "z";
+            // 
+            // xInput
+            // 
+            this.xInput.Location = new System.Drawing.Point(67, 27);
+            this.xInput.Name = "xInput";
+            this.xInput.Size = new System.Drawing.Size(100, 23);
+            this.xInput.TabIndex = 4;
+            // 
+            // yInput
+            // 
+            this.yInput.Location = new System.Drawing.Point(67, 56);
+            this.yInput.Name = "yInput";
+            this.yInput.Size = new System.Drawing.Size(100, 23);
+            this.yInput.TabIndex = 5;
+            // 
+            // zInput
+            // 
+            this.zInput.Location = new System.Drawing.Point(67, 85);
+            this.zInput.Name = "zInput";
+            this.zInput.Size = new System.Drawing.Size(100, 23);
+            this.zInput.TabIndex = 6;
+            // 
+            // resultInput
+            // 
+            this.resultInput.Location = new System.Drawing.Point(67, 114);
+            this.resultInput.Name = "resultInput";
+            this.resultInput.ReadOnly = true;
+            this.resultInput.Size = new System.Drawing.Size(100, 23);
+            this.resultInput.TabIndex = 7;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(197, 40);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 1;
+            this.addButton.Text = "Add";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // multiplyButton
+            // 
+            this.multiplyButton.Location = new System.Drawing.Point(197, 69);
+            this.multiplyButton.Name = "multiplyButton";
+            this.multiplyButton.Size = new System.Drawing.Size(75, 23);
+            this.multiplyButton.TabIndex = 2;
+            this.multiplyButton.Text = "Multiply";
+            this.multiplyButton.UseVisualStyleBackColor = true;
+            this.multiplyButton.Click += new System.EventHandler(this.multiplyButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(197, 98);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 173);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.zInput);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.multiplyButton);
+            this.Controls.Add(this.yInput);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.resultInput);
+            this.Controls.Add(this.xInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox xInput;
+        private System.Windows.Forms.TextBox yInput;
+        private System.Windows.Forms.TextBox zInput;
+        private System.Windows.Forms.TextBox resultInput;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button multiplyButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ lab1/Z1/Z1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Z1 {
+  public partial class Form1 : Form {
+    public Form1() {
+      InitializeComponent();
+    }
+
+    private void addButton_Click(object sender, EventArgs e) {
+      try {
+        resultInput.Text = (
+          Double.Parse(xInput.Text) +
+          Double.Parse(yInput.Text) +
+          Double.Parse(zInput.Text)
+          ).ToString();
+      } catch (Exception) {
+        MessageBox.Show("Provided wrong data format, check your inputs", "Error", MessageBoxButtons.OK);
+      }
+    }
+
+    private void multiplyButton_Click(object sender, EventArgs e) {
+      try {
+        resultInput.Text = (
+          Double.Parse(xInput.Text) *
+          Double.Parse(yInput.Text) *
+          Double.Parse(zInput.Text)
+          ).ToString();
+      } catch (Exception) {
+        MessageBox.Show("Provided wrong data format, check your inputs", "Error", MessageBoxButtons.OK);
+      }
+    }
+
+    private void exitButton_Click(object sender, EventArgs e) {
+      Application.Exit();
+    }
+  }
+}
